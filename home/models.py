c850dbe0e562b06b91170f7b2569c4acd2cc0bfd
firatatalay34 <firatatalay34@hotmail.com>
2020-04-12 17:51:48 +0300

    Contact Form
    Contact Form oluşturuldu.

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.forms import TextInput, ModelForm, Textarea
 
 
 class Setting(models.Model):
@@ -33,3 +34,34 @@ class Setting(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ContactFormMessage(models.Model):
+        STATUS = (
+            ('New', 'New'),
+            ('Read', 'Read'),
+            ('Closed', 'Closed'),
+        )
+        name = models.CharField(blank=True, max_length=20)
+        email = models.CharField(blank=True, max_length=50)
+        subject = models.CharField(blank=True, max_length=50)
+        message = models.CharField(blank=True, max_length=255)
+        status = models.CharField(max_length=10, choices=STATUS, default='New')
+        ip = models.CharField(blank=True, max_length=20)
+        note = models.CharField(blank=True, max_length=100)
+        create_at = models.DateTimeField(auto_now_add=True)
+        update_at = models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+            return self.name
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': TextInput(attrs={'class': 'input',}),
+            'subject': TextInput(attrs={'class': 'input',}),
+            'email': TextInput(attrs={'class': 'input',}),
+            'message': Textarea(attrs={'class': 'input', 'rows':'5'}),
+        }
