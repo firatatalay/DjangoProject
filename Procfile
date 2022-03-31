7bcf27886609bf80fc5fe94113c19228b668d172
FIRAT ATALAY <firatatalay34@hotmail.com>
2022-03-31 03:02:30 +0300

    Heroku Test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangoActivityProject.wsgi --log-file -
--- djangoActivityProject/settings.py
+++ djangoActivityProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3fanwg-%#13z1llj-gqe&n=ye%s)ynvtl^-li36g9ml&kkvdj8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['firat-djangoapp.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
